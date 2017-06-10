5711dc8d11b196a02496de56e2f4c90d4f895785
Jesse Huang <jessehuang2222@gmail.com>
2017-06-11 04:26:43 +0800

    add TCP module

---
+++ tcp/codes.java
@@ -0,0 +1,11 @@
+package tcp;
+
+public class codes {
+    public static final int TURNEAST = 0x0;
+    public static final int TURNSOUTH = 0x1;
+    public static final int TURNNORTH = 0x2;
+    public static final int TURNWEST = 0x3;
+    public static final int GET = 0x4;
+    private codes() {};
+}
+
