7f931a658371d7669ffc781f61cf2c23af9d9db6
kevin <hk850522@gmail.com>
2017-06-21 09:30:54 +0800

    Add Monster and Player in entity

---
+++ entity/Monster.java
@@ -0,0 +1,78 @@
+package entity;
+
+import java.awt.Point;
+
+import sdm.SDM;
+
+public class Monster {
+	
+	private int _max_health;
+	private int _health;
+	private Point _pos, _dir;
+	private Collider _collider;
+	
+	private int _asset_index;
+	
+	private Emitter _emitter;
+	private Long _last_move_time, _speed;
+	
+	public Monster(int h, Point pos, Point dir, int index, Emitter emitter, Long speed, Collider collider) {
+		Init(h, pos, dir, index, emitter, speed, collider);
+	}
+	
+	public Monster(int h, int index, Emitter emitter, Long speed, Collider collider) {
+		Init(h, emitter.getPosition(), emitter.getDirection(), index, emitter, speed, collider);
+	}
+	
+	private void Init(int h, Point pos, Point dir, int index, Emitter emitter, Long speed, Collider collider) {
+		_health = _max_health = h;
+		_pos = pos;
+		_dir = dir;
+		_asset_index = index;
+		_emitter = emitter;
+		_speed = speed;
+		_last_move_time = System.currentTimeMillis();
+		_collider = collider;
+	}
+	
+	private boolean canMove() {
+		if ( System.currentTimeMillis() - _last_move_time >= _speed ) {
+			_last_move_time = System.currentTimeMillis();
+			return true;
+		}
+		return false;
+	}
+	
+	public void nextPosition() {
+		if ( canMove() ) {
+			changePosition(new Point(_pos.x + _dir.x, _pos.y + _dir.y));
+		}
+	}
+	
+	public void attack() {
+		_emitter.attack();
+	}
+	
+	public void changeHealth(int delta) {
+		_health += delta;
+		if ( _health < 0 ) _health = 0;
+		if ( _health > _max_health ) _health = _max_health;
+	}
+	
+	private void changePosition(Point pos) {
+		if ( SDM.getInstance().isWalkable(pos.x, pos.y) ) {
+			System.out.println("HI");
+			_pos = pos; 
+		}
+	}
+	
+	public Collider getCollider() {
+		return _collider;
+	}
+	
+	public void Print() {
+		System.out.println("Monster : ");
+		System.out.print("Position : ");
+		System.out.println(_pos.x + " " + _pos.y);
+	}
+}
