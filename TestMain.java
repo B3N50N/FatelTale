f28a1b722d7a38d19de580441a1ee1c7bb4511f2
kevin <hk850522@gmail.com>
2017-06-24 19:17:46 +0800

    Remove file from Entity folder

--- TestMain.java
+++ TestMain.java
@@ -22,12 +22,29 @@ public class TestMain {
 		e.setPosition(new Point(100, 2000));
 		e.Print();
 		*/
-		
+		/*
 		MonsterInfo.getInstance().loadMonsterData("./resource/Data/Monster/Mode1/");
 		Monster m = MonsterInfo.getInstance().getRandomMonster();
 		m.Print();
 		m.setPosition(new Point(100, 30));
 		m.Print();
+		*/
+		/*
+		FileReader fr = new FileReader("./resource/Data/TestCollider.txt");
+		BufferedReader br = new BufferedReader(fr);
+		Collider c1 = ColliderInfo.getInstance().getCollider(br);
+		
+		fr = new FileReader("./resource/Data/TestCollider_1.txt");
+		br = new BufferedReader(fr);
+		Collider c2 = ColliderInfo.getInstance().getCollider(br);
+		
+		c1.setDirection(new Point(1, 0));
+		c1.setPosition(new Point(120, 50));
+		c2.setDirection(new Point(0, -1));
+		c2.setPosition(new Point(50,50));
+		
+		System.out.println(c1.isCollide(c2));
+		*/
 		/*
 		PEMThread t = new PEMThread();
 		t.start();
