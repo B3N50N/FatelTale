80192fc6c34ffc14c89ca21d8288d57ba4adb693
kevin <hk850522@gmail.com>
2017-06-25 23:37:46 +0800

    Fix ADM

--- adm/ADM.java
+++ adm/ADM.java
@@ -141,7 +141,6 @@ public class ADM {
 		assert i >= 0 && i < _monster_assets[i].length : "Invalid Index.";
 		return _monster_assets[index][i];
 	}
-	
 	public BufferedImage getProjectorAsset(int index, int i) {
 		assert index >= 0 && index < _projector_assets.length : "Invalid Index.";
 		assert i >= 0 && i < _projector_assets[i].length : "Invalid Index.";
