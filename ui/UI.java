6676323a1bd5bd742bccc496fb0ee6370c6f4194
Michael <Nyy20715130137@gmail.com.tw>
2017-06-26 00:09:37 +0800

    Modified back to correct version of UI.java

--- ui/UI.java
+++ ui/UI.java
@@ -7,28 +7,44 @@ import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.image.BufferStrategy;
+import java.util.HashMap;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-
+import tcp.codes;
 import tcp.TCPClient;
 
 class KeyBoardListener implements KeyListener
 {
+	final static int upcode=38,downcode=40,rightcode=39,leftcode=37;
+	final static int attackcode=87;
+	final HashMap<Integer,Integer> codetable;
 	public void keyTyped(KeyEvent e)
 	{
+		
 	}
 	public void keyReleased(KeyEvent e)
-	{	
+	{
+		int code=e.getKeyCode();
+		//TCPClient.getClient().keyRelease(code);
 	}
 	public void keyPressed(KeyEvent e)
 	{
-		//int movecode=e.getKeyCode();
-		//call keyGetPressed()
-		//call inputMoves(movecode)
+		int code=e.getKeyCode();
+		//TCPClient.getClient().keyDown(code);
+		//TCPClient.getClient().keyDown(codetable.get(code));
+	}
+	public KeyBoardListener()
+	{
+		codetable=new HashMap();
+		codetable.put(upcode,codes.MOVEUP);
+		codetable.put(leftcode,codes.MOVELEFT);
+		codetable.put(rightcode,codes.MOVERIGHT);
+		codetable.put(downcode,codes.MOVEDOWN);
+		codetable.put(attackcode, codes.ATTACK);
 	}
 }
 public class UI
@@ -38,6 +54,9 @@ public class UI
 	private Canvas canvas;
 	private final int framewidth=500,frameheight=500;
 	private BufferStrategy bs;
+	private JPanel startmenupanel,waitingpanel;
+	private JPanel endgamepanel,gamepanel;
+    private String srvaddr;
 	private UI()
 	{
 		frame=new JFrame();
@@ -45,8 +64,45 @@ public class UI
 		frame.setSize(framewidth,frameheight);
 		frame.setVisible(true);
 		frame.setResizable(false);
+		buildStartMenuPanel();
+		
 	}
-	public static synchronized UI getinstance()
+	private void buildStartMenuPanel()
+	{
+		startmenupanel=new JPanel();
+		assert startmenupanel!=null:"startmenupanel is null";
+		startmenupanel.setLayout(null);
+		JButton button=new JButton("Start Game");
+		button.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent e)
+			{
+				waitingScreen();
+                new Thread() {
+                    public void run() {
+                        TCPClient.getClient().connectServer(srvaddr);
+                    }
+                }.start();
+			}
+		}
+		);
+		button.setBounds(150,100, 150,50);
+		startmenupanel.add(button);
+		frame.setVisible(true);
+		button=new JButton("Exit");
+		button.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent e)
+			{
+				frame.dispose();
+			}
+		}
+		);
+		button.setBounds(150, 250,150,50);
+		startmenupanel.add(button);
+		frame.setVisible(true);
+	}
+	public static synchronized UI getInstance()
 	{
 		if(uniqueinstance==null)
 		{
@@ -76,51 +132,23 @@ public class UI
 		else
 			return bs;
 	}
-	public void startMenu(String srvaddr)
+	public void startMenu(String _srvaddr)
 	{
+        srvaddr = _srvaddr;
 		frame.remove(frame.getContentPane());
-		frame.add(new JPanel());
-		frame.getContentPane().setLayout(null);
-		JButton button=new JButton("Start Game");
-		button.addActionListener(new ActionListener()
-		{
-			public void actionPerformed(ActionEvent e)
-			{
-				waitingScreen();
-                new Thread() {
-                    public void run() {
-                        TCPClient.getClient().connectServer(srvaddr);
-                    }
-                }.start();
-			}
-		}
-		);
-		button.setBounds(150,100, 150,50);
-		frame.getContentPane().add(button);
-		frame.setVisible(true);
-		button=new JButton("Exit");
-		button.addActionListener(new ActionListener()
-		{
-			public void actionPerformed(ActionEvent e)
-			{
-				frame.dispose();
-			}
-		}
-		);
-		button.setBounds(150, 250,150,50);
-		frame.getContentPane().add(button);
-		frame.setVisible(true);
+		assert startmenupanel!=null:"startmenupanel is null";
+		frame.add(startmenupanel);
 	}
 	public void startGame()
 	{
 		canvas=new Canvas();
-		frame.getContentPane().removeAll();
+		frame.remove(frame.getContentPane());
 		frame.add(new JPanel());
 		canvas=new Canvas();
 		canvas.setBounds(0,0,framewidth,frameheight);		
 		frame.add(canvas);
 		frame.addKeyListener(new KeyBoardListener());
-		System.out.println(frame.getWidth());
+		frame.setVisible(true);
 	}
 	public int getCanvasWidth()
 	{
@@ -133,15 +161,17 @@ public class UI
 	public void waitingScreen()
 	{
 		frame.getContentPane().removeAll();
-		frame.getContentPane().add(new JPanel());
+		frame.add(new JPanel());
 		frame.getContentPane().repaint();
 		JLabel lbl=new JLabel();
-		lbl.setIcon(new ImageIcon(this.getClass().getResource("../resource/Images/waitingscreen.jpg")));
+		lbl.setIcon(new ImageIcon(this.getClass().getResource("../resource/waitingscreen.jpg")));
 		lbl.setBounds(0, 0,500,500);
 		frame.getContentPane().add(lbl);
 		frame.getContentPane().setLayout(null);
 	}
-	public static void main(String[] args)
+	/*public static void main(String[] args)
 	{
-	}
+		UI demo=new UI();
+		demo.startGame();
+	}*/
 }
